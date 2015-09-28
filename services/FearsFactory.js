5ed27ba6a4e61f4f825773b1643ebd6bd4353582
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-28 15:40:35 -0700

    adds form for submitting a fear; adds iteration to show all fears

--- services/FearsFactory.js
+++ services/FearsFactory.js
@@ -2,7 +2,7 @@ fearsLister.factory('FearsFactory', function FearsFactory(){
   var factory = {};
   factory.fears = [];
   factory.addFear = function(){
-    factory.fears.push({name: factory.fearName, id: factory.fears.length + 1, people: []});
+    factory.fears.push({name: factory.fearName, id: factory.fears.length + 1, members: []});
     factory.fearName = null;
   };
   return factory;
