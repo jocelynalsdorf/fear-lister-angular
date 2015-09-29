a48ac9c976ac113a001f38761a67a16ea6dd16a2
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-29 09:19:08 -0700

    fix bug where members were not being connected correctly with specific fear on a click of the fear

--- controllers/MembersController.js
+++ controllers/MembersController.js
@@ -1,5 +1,5 @@
 fearsLister.controller('MembersCtrl', function MembersCtrl($scope, $stateParams, FearsFactory, UtilitiesFactory){
-  $scope.member = UtilitiesFactory.findById(FearsFactory.fears, $stateParams.courseId);
+  $scope.fear = UtilitiesFactory.findById(FearsFactory.fears, $stateParams.fearId);
   $scope.addMember =  function(){
     $scope.fear.members.push({name: $scope.memberName});
     $scope.memberName = null;
